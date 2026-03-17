ad324a5b0eeef14ea1bb6434964e2e224addb65b
Lokesh <lokeshtewatia003@gmail.com>
2026-03-17 21:22:11 +0530

    add

---
+++ Sorting/pattern4.java
@@ -0,0 +1,23 @@
+public class pattern4 {
+    public static void main(String[] args) {
+        int rows = 6; 
+
+        for (int i = 1; i <= rows; i++) {
+        
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+        
+            System.out.println();
+        }
+    }
+}
+
+
+// Output :-
+// *
+// **
+// ***
+// ****
+// *****
+// ******
